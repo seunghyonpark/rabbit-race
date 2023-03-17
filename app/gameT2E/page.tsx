ccb125df41b76957bb3b9ab0dc517a19586ccbff
wayne309 <wayne@nuklabs.com>
2023-03-17 17:16:35 +0900

    good

--- app/gameT2E/page.tsx
+++ app/gameT2E/page.tsx
@@ -5,7 +5,7 @@ import Son20Oyun from '@/components/betScreen/son20';
 import LatestWinners from '@/components/betScreen/latestWinners';
 import YuruyenAt from '@/components/betEkrani/yuruyenAt'
 
-import Race from '@/components/yarisEkrani/RaceGame';
+import Race from '@/components/yarisEkrani/raceGame';
 
 import SocketEnum from '@/libs/enums/socket';
 import React, { useEffect, useState, useMemo } from 'react';
