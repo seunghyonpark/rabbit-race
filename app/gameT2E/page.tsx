36fef22872adf415dd187725c72667ce10c46bf7
wayne309 <wayne@nuklabs.com>
2023-03-30 14:23:42 +0900

    jasonny update

--- app/gameT2E/page.tsx
+++ app/gameT2E/page.tsx
@@ -745,7 +745,7 @@ useEffect(() => {
                                     className={`flex items-center justify-center text-l  bg-black rounded-md h-[36px] text-center px-5 text-[#BA8E09] border border-[#BA8E09] mt-1`}
                                 >
                                    <span className="text-[#ffffff] text-sm">PRICE (ETH):</span>&nbsp;&nbsp;&nbsp;
-                                   <span className="text-xl">{Number(currentPrice).toFixed(2)}&nbsp;&nbsp;&nbsp;</span>
+                                   <span className="text-sm">{Number(currentPrice).toFixed(2)}&nbsp;&nbsp;&nbsp;</span>
                                    <span className="text-[#ffffff] text-sm">USDT</span>
                                 </div>
 
