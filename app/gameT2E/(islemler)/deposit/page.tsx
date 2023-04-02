970d8a6a464097e7fc35d827e386f7c7586b89a0
wayne309 <wayne@nuklabs.com>
2023-04-02 15:46:23 +0900

    good update

--- app/gameT2E/(islemler)/deposit/page.tsx
+++ app/gameT2E/(islemler)/deposit/page.tsx
@@ -560,6 +560,8 @@ export default function Deposit() {
           in your account
         </p>
         <div className="p-2 grid grid-cols-1 lg:grid-cols-2 w-full lg:w-2/3 gap-5">
+
+          
           {/* //? Matic Deposit  */}
           <div className="w-full border rounded-lg flex flex-col items-center justify-center p-2 gap-5 py-10">
             <h4 className=" ">
