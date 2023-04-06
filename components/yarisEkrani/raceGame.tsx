e23e087e5ef32f8a3bab68095166dd17d0e437e9
wayne309 <wayne@nuklabs.com>
2023-04-06 14:53:12 +0900

    long time no see

--- components/yarisEkrani/raceGame.tsx
+++ components/yarisEkrani/raceGame.tsx
@@ -588,9 +588,9 @@ export default function Race({socket, username, currentPrice, betPrice}: {socket
                     </div>
                             */}
 
-<div className="w-full ">
 
-<div className="w-full h-6 mt-0"
+
+<div className="w-full h-8 mt-0"
     style={{
         //backgroundImage: `url('/cracle-banner.png')`,
         backgroundImage: `url('/cracle-banner.png')`,
@@ -600,7 +600,7 @@ export default function Race({socket, username, currentPrice, betPrice}: {socket
     }}
 ></div>
                     
-</div>    
+ 
 
 
                     <div className="w-full h-5 mt-0 mb-5">
