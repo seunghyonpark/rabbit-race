e0e715956778dc95b4225d730e0f8bbca1b26efa
wayne309 <wayne@nuklabs.com>
2023-04-07 17:57:31 +0900

    second commit

--- libs/enums/domain.ts
+++ libs/enums/domain.ts
@@ -1,9 +1,11 @@
 enum DomainEnum {
   
-  address = "http://localhost:3000",
+  ///address = "http://localhost:3000",
   
   
   ///address = "https://craclegames.nuklabs.xyz",
+
+  address = "https://demo.craclegamez.io",
 
 }
 
